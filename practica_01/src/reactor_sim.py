92d9ae8d8facb68035d426a9be4c482257917761
Eduardo-Ramoss <zs22013532@estudiantes.uv.mx>
2026-09-07 12:48:24 -0600

    Se agrego clase
    Se agrego la Clase Actuador en donde se definen los atributos del Actuador

--- practica_01/src/reactor_sim.py
+++ practica_01/src/reactor_sim.py
@@ -15,3 +15,14 @@ def limpiar_pantalla():
     """Limpia la terminal según el sistema operativo (cls para Windows, clear para Unix)."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# ==============================================================================
+# AQUI AGREGAMOS LA CLASE ACTUADOR
+# ==============================================================================
+class Actuador:
+    def __init__(self, nombre: str):
+        # Atributos de estado del actuador
+        self.nombre = nombre
+        self.rango_operacion_min = 0.0   # Límite mínimo de operación (0%)
+        self.rango_operacion_max = 100.0 # Límite máximo de operación (100%)
+        self.estado = False              # Estado lógico de encendido: False = OFF, True = ON
+        self.punto_operacion = 0.0       # Porcentaje actual de operación
